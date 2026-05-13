dc4dede1decf6ae736a4c86df1f941fcd2e0ab3a
Rahul bisht <rahul.bisht@telgoo5.com>
2026-05-13 10:47:16 +0530

    intial tax and price calculations

--- Practice Project Price Calculator/main.go
+++ Practice Project Price Calculator/main.go
@@ -1,3 +1,19 @@
 package main
 
-func main() {}
+import "fmt"
+
+func main() {
+	prices := []float64{10, 20, 30}
+	taxes := []float64{0, 0.07, 0.1, 0.15}
+	results := make(map[float64][]float64)
+
+	for _, valueTaxed := range taxes {
+		tempArray := []float64{}
+		for _, valuePrices := range prices {
+			tempArray = append(tempArray, valuePrices-valuePrices*valueTaxed)
+		}
+		results[valueTaxed] = tempArray
+	}
+
+	fmt.Println(results)
+}
