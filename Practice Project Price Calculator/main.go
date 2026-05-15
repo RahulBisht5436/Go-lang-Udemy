5404644c9f4f6edc61715f0d58ed72b8c2435a6a
Rahul bisht <rahul.bisht@telgoo5.com>
2026-05-15 09:42:52 +0530

    Changes for the Reader utlity function

--- Practice Project Price Calculator/main.go
+++ Practice Project Price Calculator/main.go
@@ -1,18 +1,35 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+
+	"example.com/price_calculator/prices"
+	"example.com/price_calculator/utility"
+)
 
 func main() {
-	prices := []float64{10, 20, 30}
-	taxes := []float64{0, 0.07, 0.1, 0.15}
-	results := make(map[float64][]float64)
+	priceInfoRawContent, priceInfoRawErr := os.ReadFile("prices.txt")
+	if priceInfoRawErr != nil {
+		fmt.Println("Error occured in Reading File")
+		return
+	}
+	pricesInfo, priceInfoErr := utility.FileBytesToSlice(priceInfoRawContent)
+	if priceInfoErr != nil {
+		fmt.Println(priceInfoErr.Error())
+	}
 
-	for _, valueTaxed := range taxes {
-		tempArray := []float64{}
-		for _, valuePrices := range prices {
-			tempArray = append(tempArray, valuePrices-valuePrices*valueTaxed)
-		}
-		results[valueTaxed] = tempArray
+	//  pricesInfo:= []float64{10, 20, 30}
+	taxes := []float64{0, 0.07, 0.1, 0.15}
+	fmt.Println("line no 13 printed ")
+	results, err := prices.CalcPrices(pricesInfo, taxes)
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+	for _, taxRate := range taxes {
+		priceJob := prices.NewTaxIncludedPriceJob(taxRate)
+		priceJob.Process()
 	}
 
 	fmt.Println(results)
